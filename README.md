3afa9a792672721085ac979c0fdd78569b000e08
gsimon29 <gael.simon29@gmail.com>
2017-06-22 10:10:28 +0200

    test1 commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# rde-python-parallel
-Python scripts that use parallel computing to approximate the solution of Reaction-Diffusion Equations
+---
+output:
+  pdf_document: default
+  html_document: default
+---
+# github.com/gsimon29/rde-python-parallel
+Python scripts that use parallel computing to approximate the solution of Reaction-Diffusion Equations.
+
+__rde_btcs_parallel.py__ : 
+Numerical approximation with parallel computing of the reaction-diffusion equation.
+Dirichlet boundary conditions and the method BTCS (Backward-Time Central-Space) are used.
+The equation is solved with and without the reaction term.
+The transition matrix from a time step to another is sparsed with the function scipy.sparse.diags .
+The output files are csv files and it is possible to visualize the solutions with a mp4 movie. 
--- rde_btcs_parallel.py
+++ rde_btcs_parallel.py
@@ -1,10 +1,9 @@
 """
-Approximation numérique en parallèle de l’équation de réaction-diffusion avec condition
-de Dirichlet avec la méthode BTCS (Backward-Time Central-Space).
-L'équation est résolu avec et sans le terme de réaction.
-La matrice de passage d’un pas de temps à un autre est dispersée avec la fonction scipy.sparse.diags .
-Les résultats sont visualisés sous forme de film mp4
-
+Numerical approximation with parallel computing of the reaction-diffusion equation.
+Dirichlet boundary conditions and the method BTCS (Backward-Time Central-Space) are used.
+The equation is solved with and without the reaction term.
+The transition matrix from a time step to another is sparsed with the function scipy.sparse.diags .
+The output files are csv files and it is possible to visualize the solutions with a mp4 movie. 
 """
 
 # -*- coding: utf-8 -*-
@@ -13,6 +12,7 @@ from __future__ import division
 from mpi4py import MPI
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import scipy.sparse
 import scipy.sparse.linalg
 import os
@@ -21,161 +21,164 @@ import time
 
 def Sol_exa(t,X,xmax) :
   """
-  Calcul la valeur exacte de la solution à un temps t si elle
-  est connue et rentrée par l'utilisateur.
-
+  Compute the exact solution at a time step t if the solution
+  is known and given par the user
+  
   Arguments:
-    t (float) : Temps.
-    X (array) : Points aux quels la solution va être calculée
-    xmax (float) : Borne supérieur de l'intervalle de définition de l'espace
+    t (float) : Time.
+    X (array) : Space interval or space sub-interval
+    xmax (float) : Right boundary of the space interval
 
-  Retour:
-    array : Valeurs de la solution au temps t donné
-
+  Returns:
+    array : Values of the solution at the given time step t
   """
   
-  #Solution exacte pour CI sin(pi*x/x_max) 
+  #Exact solution for IC sin(pi*x/x_max) 
   return np.sin(np.pi*X/xmax)*np.exp(-np.pi**2*t/xmax**2)
 
 def Grap(space,X1,X2,t,x0,xmax,ymin,ymax,i,rank) :
   """
-  Créer le graphe de la solution trouvée à l'instant t et le sauvegarde.
+  Create the graph of the solution at a given time step t and save it.
 
   Arguments:
-    space (array(1,n)) : Intervalle [x0,xmax]
-    X1,X2 (array(1,n)) : Solutions approchées trouvées à l'instant t, doivent être de même longueur que space
-    t (float) : Temps
-    x0,xmax (float) : Borne inférieur et supérieur de l'intervalle de définition de l'espace
-    ymin,ymax (float) : Valeur minimale et maximale de toute les solutions
-    i (int) : Sera inclut dans le nom du fichier du graphe
-    rank (int) : Rang du coeur qui fait tourner le programme
+    space (array(1,n)) : Interval [x0,xmax]
+    X1,X2 (array(1,n)) : Numerical solutions found at the time step t, their lenght must be equal to the lenght of space
+    t (float) : Time
+    x0,xmax (float) : Left and right boundaries of the space interval
+    ymin,ymax (float) : Minimal value and maximal value of the solutions
+    i (int) : Used to name the graph file
+    rank (int) : Rank of the core that run the program
   """
   
   SolExa = Sol_exa(t,space,xmax)
-  plt.plot(space,SolExa,label="Solution exacte sans réaction")
-  plt.plot(space,X1,label="Approximation sans réaction")
-  plt.plot(space,X2,label="Approximation avec réaction u(1-u)")
+  plt.plot(space,SolExa,label="Exact solution without reaction")
+  plt.plot(space,X1,label="Numerical solution without reaction")
+  plt.plot(space,X2,label="Numerical solution with reaction exp(-u)")
   plt.text(x0 + (xmax-x0)/10,ymax, "t=" + str("%.3f"% t) + "s", horizontalalignment = 'center', verticalalignment = 'center')
-  plt.title ("Simulation de l'equation de réaction-diffusion avec condition de Dirichlet")
-  plt.xlabel ( 'Espace')
+  plt.title ("Numerical solution of the reaction-diffusion equation with Dirichlet BC")
+  plt.xlabel ( 'Space')
   plt.ylabel ( 'Concentration')
   plt.ylim(ymin,ymax + 0.1*abs(ymax))
   plt.xlim(x0,xmax)
-  plt.legend()
-  plt.savefig('edp' + str(rank) + "0"*(4-len(str(i))) + str(i) + '.png', transparent=False)
+  plt.legend(loc=1,prop={'size':7.5})
+  plt.savefig('rde' + str(rank) + "0"*(5-len(str(i))) + str(i) + '.png', transparent=False)
   plt.clf()
 
 def Film(matrix1,matrix2,pas_t,times,space,x0,xmax,rank,size) :
   """
-  Créer un film qui permet de visualiser les résultats obtenue.
+  Create a movie to visualize the results
 
   Arguments:
-    matrix1,matrix2 (array(n,m)) : Solutions approchées trouvées à tout les instants t
-    pas_t (int) : Nombre de points utilisés pour la subdivision du temps
-    times (array(1,n)) : Intervalle du temps
-    space (array(1,m)) : Intervalle [x0,xmax]
-    x0,xmax (float) : Borne inférieur et supérieur de l'intervalle de définition de l'espace
-    rank (int) : Rang du coeur qui fait tourner le programme
-    size (int) : Nombre de coeurs qui font tourner le programme en même temps
+    matrix1,matrix2 (array(n,m)) : Numerical solutions
+    pas_t (int) : Number of points used to divide the time interval
+    times (array(1,n)) : Time
+    space (array(1,m)) : Interval [x0,xmax]
+    x0,xmax (float) : Left and right boundaries of the space interval
+    rank (int) : Rank of the core that run the program
+    size (int) : Number of cores that run the program in the same time
   """
-  #Calcul le minimum et le maximum de la solution trouvé 
+  #Compute the minimum and the maximum of the solutions
   y_min = min(matrix1.min(),matrix2.min())
   y_max = max(matrix1.max(),matrix2.max())
   
-  #Crée les plots pour chaque pas de temps 
-  for i in range((rank*pas_t)//size,((rank + 1)*pas_t)//size) :
+  #Create a graph for some time steps with a constant gap
+  gap=40
+  for j in range(rank*pas_t//(gap*size),(rank + 1)*pas_t//(gap*size)) :
+    i=gap*j
     Grap(space,matrix1[i],matrix2[i],times[i],x0,xmax,y_min,y_max,i,rank)
   
-  #Converti les images des plots en .mp4
-  cmd = 'convert edp' + str(rank) + '*.png film-edp-be' + str(rank) + '.mp4'
+  #Convert the images into a mp4 file
+  cmd = 'convert rde' + str(rank) + '*.png movie-rde-btcs' + str(rank) + '.mp4'
   os.system(cmd)
   
-  #Supprime les images
-  for i in range((rank*pas_t)//size,((rank + 1)*pas_t)//size) :
-    os.remove('edp' + str(rank) + "0"*(4-len(str(i))) + str(i) + '.png') 
+  #Delete the images
+  for j in range(rank*pas_t//(gap*size),(rank + 1)*pas_t//(gap*size)) :
+    i=gap*j
+    os.remove('rde' + str(rank) + "0"*(5-len(str(i))) + str(i) + '.png') 
   
-  comm.allgather(1) #Permet de lancer l'étape suivante quand tous les coeurs ont fini la précédente
+  comm.allgather(1) #It enable to run the next step when the cores have finished the previous one
   
   if rank==0 :
-    #Assemble les .mp4 obtenues avec chaque coeur
-    cmd1 = 'convert film-edp-be*.mp4 edp-reac-be.mp4'
+    #Gather the mp4 files into one
+    cmd1 = 'convert movie-rde-btcs*.mp4 movies-rde-btcs.mp4'
     os.system(cmd1)
   
-    #Supprime les .mp4 intermédiaires
+    #Delete the temporary mp4 files
     for i in range(size) :
-      os.remove('film-edp-be' + str(i) + '.mp4')
+      os.remove('movie-rde-btcs' + str(i) + '.mp4')
 
 
 def CI(x,xmax) :
   """
-  Fonction qui calcule les valeurs pour la condition initiale
+  Function that compute the initial condition
 
   Arguments:
-    x (array(1,n)) : Intervalle ou sous-intervalle de l'espace
-    xmax (float) : Borne supérieur de l'intervalle de définition de l'espace
+    x (array(1,n)) : Space interval or space sub-interval
+    xmax (float) : Right boundary of the space interval
     
-  Retour:
-    array(1,n) : Solutions obtenues
+  Returns:
+    array(1,n) : 
   """
   return np.sin(np.pi*x/xmax)
   
 def Reac(u) :
   """
-  Fonction qui calcule les valeurs pour la réaction
+  Function that compute the reaction term
 
   Arguments:
-    u (array(1,n)) : Solutions à un pas de temps donné
+    u (array(1,n)) : Solutions at a given time step t
     
-  Retour:
-    array(1,n) : Valeurs de la réaction
+  Returns:
+    array(1,n) : Values of the reaction
   """
-  return u*(1-u)
+  return np.exp(-u)
 
-#Prise du temps initial
+#Initial time taking
 start_time = time.time()
 
-#Initiation du parallelisme
+#Initiation of the parallel computing
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
   
-#Définition du temps
+#Time definition
 t0 = 0
 tmax = 1
-pas_t = 500
+pas_t = 5000
 delta_t = (tmax-t0)/(pas_t)
 times = np.linspace(t0,tmax,pas_t + 1)
 
-#Définition de l'espace
+#Space definition
 x0 = 0
 xmax = 2
 pas_x = 19
 
-#Pour la suite du programme, il faut que (pas_x + 1) soit divisible par size, sinon il y a un problème dans la création des matrices
+#For the good development of the program, we need (pas_x + 1) to be divisible by size,
+#it is neccesary to have good overlaps between the matrices
 if (pas_x + 1)%size != 0 :
-   pas_x = pas_x + (size-(pas_x + 1)%size)
+  pas_x = pas_x + (size-(pas_x + 1)%size)
 pas_x_s = pas_x//(size)
 delta_x = (xmax-x0)/(pas_x)
 space = np.linspace(x0,xmax,pas_x + 1)
 
-#Définition des paramètres
+#Parameters definition
 k = 1
 r = k*delta_t/delta_x**2
 
-#Creation de la matrice qui va contenir les résultats
+#Creation of the matrix that will contain the results
 final_matrix1 = np.zeros((pas_t + 1,pas_x_s + 4))
 final_matrix2 = np.zeros((pas_t + 1,pas_x_s + 4))
 
-#Condition initiales
+#Initial conditions
 matrix1 = CI(space[rank*pas_x_s:rank*pas_x_s + pas_x_s + 4],xmax)
 matrix2 = CI(space[rank*pas_x_s:rank*pas_x_s + pas_x_s + 4],xmax)
-final_matrix1[0][:] = matrix1[:]
-final_matrix2[0][:] = matrix2[:]
+final_matrix1[0,:] = matrix1[:]
+final_matrix2[0,:] = matrix2[:]
 
 b1 = np.zeros(pas_x_s + 4)
 b2 = np.zeros(pas_x_s + 4)
 
-#Création des matrices qui vont permettre de disperser la matrice A
+#Creation of the matrices that will be used to sparse the matrix A
 main  = np.zeros(pas_x_s + 4)
 lower = np.zeros(pas_x_s + 3)
 upper = np.zeros(pas_x_s + 3)
@@ -183,7 +186,7 @@ main[:] = 1 + 2*r
 lower[:] = -r 
 upper[:] = -r  
 
-# Insertion des conditions aux bords
+#Implementation of boundary conditions (Dirichlet)
 if rank == 0 :
   main[0] = 1
   upper[0] = 0
@@ -192,7 +195,7 @@ if rank == size-1 :
   main[pas_x_s + 3] = 1
   lower[pas_x_s + 2] = 0
 
-#Dispersion de la matrice A
+#Sparsing of the matrix A
 A = scipy.sparse.diags(
     diagonals = [main, lower, upper],
     offsets = [0, -1, 1], shape = (pas_x_s + 4, pas_x_s + 4),
@@ -200,41 +203,41 @@ A = scipy.sparse.diags(
     
 #print(rank,A.todense(),"\n")
 
-#Calcul de la solution en parallèle avec condition de Dirichlet
-for n in range(0, pas_t) :
+#Compute the solution in parallel 
+for n in range(0, pas_t+1) :
   if 0 < rank :
-  # Envoie matrix[2] et matrix[3] à rank-1
+  # Send matrix[2] and matrix[3] to rank-1
     comm.send(matrix1[2], dest = rank-1, tag = 1)
     comm.send(matrix1[3], dest = rank-1, tag = 2)
     comm.send(matrix2[2], dest = rank-1, tag = 3)
     comm.send(matrix2[3], dest = rank-1, tag = 4)
 
-  # Recoit matrix[pas_x_s + 2] et matrix[pas_x_s + 3] de rank + 1
+  # Receive matrix[pas_x_s + 2] and matrix[pas_x_s + 3] from rank+1
   if rank < size-1 :
     matrix1[pas_x_s + 2] = comm.recv(source = rank + 1, tag = 1)
     matrix1[pas_x_s + 3] = comm.recv(source = rank + 1, tag = 2)
     matrix2[pas_x_s + 2] = comm.recv(source = rank + 1, tag = 3)
     matrix2[pas_x_s + 3] = comm.recv(source = rank + 1, tag = 4)
 
-  # Envoie matrix[pas_x_s] et matrix[pas_x_s+1] à rank + 1
+  # Send matrix[pas_x_s] and matrix[pas_x_s+1] à rank+1
   if rank < size-1 :
     comm.send(matrix1[pas_x_s], dest = rank + 1, tag = 5)
     comm.send(matrix1[pas_x_s + 1], dest = rank + 1, tag = 6)
     comm.send(matrix2[pas_x_s], dest = rank + 1, tag = 7)
     comm.send(matrix2[pas_x_s + 1], dest = rank + 1, tag = 8)
 
-  # Recoit matrix[0] et matrix[1] de rank-1
+  # Receive matrix[0] and matrix[1] from rank-1
   if 0 < rank :
     matrix1[0] = comm.recv(source = rank-1, tag = 5)
     matrix1[1] = comm.recv(source = rank-1, tag = 6)
     matrix2[0] = comm.recv(source = rank-1, tag = 7)
     matrix2[1] = comm.recv(source = rank-1, tag = 8)
     
-  #Mise à jour des valeurs de la matrice b
+  #Update the values of the matrix B
   b1[:] = matrix1[:]
   b2[:] = matrix2[:] + delta_t*Reac(matrix2[:])
   
-  #Application des conditions aux bords, ici Dirichlet pour rappel
+  #Implementation of boundary conditions (Dirichlet)
   if rank == 0 :
     b1[0] = 0
     b2[0] = 0
@@ -242,15 +245,15 @@ for n in range(0, pas_t) :
     b1[pas_x_s + 3] = 0
     b2[pas_x_s + 3] = 0
     
-  #Calcul des nouvelles valeurs au pas de temps suivant
+  #Compute the values at the next time step
   matrix1[:] = scipy.sparse.linalg.spsolve(A, b1)
   matrix2[:] = scipy.sparse.linalg.spsolve(A, b2)
   
-  #Stockage des valeurs obtenues dans la matrice finale
+  #Storage of the obtained values into the final matrix
   final_matrix1[n][:] = matrix1[:]
   final_matrix2[n][:] = matrix2[:]
   
-#Garde seulement les solutions nécessaires (suppression des chevauchements entre les différentes matrices)
+#Keep only the necessary solutions (delete overlaps between the matrices)
 if rank == 0 :
   final_matrix1b = final_matrix1[:,0:pas_x_s + 2]
   final_matrix2b = final_matrix2[:,0:pas_x_s + 2]
@@ -261,16 +264,30 @@ else :
   final_matrix1b = final_matrix1[:,2:pas_x_s + 2]
   final_matrix2b = final_matrix2[:,2:pas_x_s + 2]
 
-#Collecte et assemble les matrices de chaque coeur
+#Gather the matrixes of each core
 gathered_list1 = comm.allgather(final_matrix1b)
 gathered_list2 = comm.allgather(final_matrix2b)
 gathered_matrix1 = np.concatenate(gathered_list1,axis = 1)  
-gathered_matrix2 = np.concatenate(gathered_list2,axis = 1)  
+gathered_matrix2 = np.concatenate(gathered_list2,axis = 1)
 
-#Création du .mp4 pour visualer la simulation
+#Name of the path of the output files
+path_no_reac="Results/results-rde-btcs-no-reac.csv"
+path_reac="Results/results-rde-btcs-with-reac.csv"
+
+#Create DataFrames and convert them into csv files
+round_number=4
+formats="%."+str(round_number)+"f"
+if rank==0:
+  tab1=pd.DataFrame(data=gathered_matrix1, index=np.round(times,round_number), columns=np.round(space,round_number))
+  tab1.to_csv(path_no_reac,float_format=formats)
+if rank==1:
+  tab2=pd.DataFrame(data=gathered_matrix2, index=np.round(times,round_number), columns=np.round(space,round_number))
+  tab2.to_csv(path_reac,float_format=formats)
+  
+#Create the mp4 file
 Film(gathered_matrix1,gathered_matrix2,pas_t + 1,times,space,x0,xmax,rank,size)
 
-#Prise du temps final et mesure du temps écoulé
+#Final time taking and elapsed time computation
 end_time = time.time()
 elapsed_time = end_time-start_time
-print("\nL'exécution du programme a pris", round(elapsed_time,4),"s sur le coeur ",rank)
+print("\nThe program have run for", round(elapsed_time,4),"s on the core ",rank)
